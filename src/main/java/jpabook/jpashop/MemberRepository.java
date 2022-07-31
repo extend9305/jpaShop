9194030ade912c021929fa1f7584b6eacb676788
extend9305 <extend9305@naver.com>
2022-07-31 21:10:29 +0900

    spring+jpa study 1 20220731

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {//dao
+
+    @PersistenceContext //찾아보기
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+
+
+}
