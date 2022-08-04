5daf0855606fbb91e97b6d2c12886f527454e6da
leedongsu <extend9305@naver.com>
2022-08-04 23:57:56 +0900

    2022-08-04 study

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -8,12 +8,16 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {//dao
 
-    @PersistenceContext //찾아보기
+    @PersistenceContext //찾아보기 스프링컨테이너가 주입해줌.
     private EntityManager em;
 
     public Long save(Member member){
-        em.persist(member);
+        em.persist(member); //insert 기능
         return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
     }
 
 
